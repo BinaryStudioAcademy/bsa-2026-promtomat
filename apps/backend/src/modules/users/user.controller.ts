aa528aee6c80053fe18f0a725b85189499196ec1
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-08-25 16:54:22 +0300

    feat: document GET /users security in swagger pm-8

--- apps/backend/src/modules/users/user.controller.ts
+++ apps/backend/src/modules/users/user.controller.ts
@@ -44,6 +44,8 @@ class UserController extends BaseController {
 	 * /users:
 	 *    get:
 	 *      description: Returns an array of users
+	 *      security:
+	 *        - bearerAuth: []
 	 *      responses:
 	 *        200:
 	 *          description: Successful operation
