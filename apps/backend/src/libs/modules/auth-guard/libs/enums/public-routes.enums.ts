cc0cf49168a6d379f6bc7e2029603692cd21c816
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-08-28 00:13:29 +0300

    feat: implement method support for public routes pm-8

--- apps/backend/src/libs/modules/auth-guard/libs/enums/public-routes.enums.ts
+++ apps/backend/src/libs/modules/auth-guard/libs/enums/public-routes.enums.ts
@@ -1,13 +1,18 @@
-import { APIPath, AuthApiPath } from "@promptomat/shared";
+import { type PublicRoutes } from "../types/types.js";
+import { APIPath, AuthApiPath, HTTPMethod } from "./enums.js";
 
 const API_VERSION_PREFIX = "/api/v1";
 const SWAGGER_DOCS_PREFIX = "/v1/documentation";
 
-const PUBLIC_ROUTES: readonly string[] = [
-	`${API_VERSION_PREFIX}${APIPath.AUTH}${AuthApiPath.SIGN_IN}`,
-	`${API_VERSION_PREFIX}${APIPath.AUTH}${AuthApiPath.SIGN_UP}`,
-	`${API_VERSION_PREFIX}${APIPath.HEALTH}`,
-	SWAGGER_DOCS_PREFIX,
-];
+const PUBLIC_ROUTES: PublicRoutes = {
+	[`${API_VERSION_PREFIX}${APIPath.AUTH}${AuthApiPath.SIGN_IN}`]: [
+		HTTPMethod.POST,
+	],
+	[`${API_VERSION_PREFIX}${APIPath.AUTH}${AuthApiPath.SIGN_UP}`]: [
+		HTTPMethod.POST,
+	],
+	[`${API_VERSION_PREFIX}${APIPath.HEALTH}`]: [HTTPMethod.GET],
+	[SWAGGER_DOCS_PREFIX]: [HTTPMethod.GET, HTTPMethod.POST],
+};
 
 export { PUBLIC_ROUTES };
