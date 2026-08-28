bacfd54df1c7cec386209245d5b987950a07c4a6
Blackpossum <yosaphatharwindra@gmail.com>
2026-08-28 14:25:10 +0700

    refactor: remove unused props and simplify Button component implementation pm-19

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,19 +1,11 @@
 type Properties = {
-	className?: string;
 	label: string;
-	onClick?: () => void;
 	type?: "button" | "submit";
 };
 
 const Button: React.FC<Properties> = ({
-	className,
 	label,
-	onClick,
 	type = "button",
-}: Properties) => (
-	<button className={className} onClick={onClick} type={type}>
-		{label}
-	</button>
-);
+}: Properties) => <button type={type}>{label}</button>;
 
 export { Button };
