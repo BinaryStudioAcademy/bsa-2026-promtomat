a2beffd7eb73a0380d6df30561fbc47d2a46614a
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-08-28 11:31:10 +0300

    refactor: rename file to public-routes.enum.js pm-8

--- apps/backend/src/libs/modules/auth-guard/libs/enums/enums.ts
+++ apps/backend/src/libs/modules/auth-guard/libs/enums/enums.ts
@@ -1,4 +1,4 @@
 export { AuthErrorMesssage } from "./auth-error-messages.enum.js";
 export { BEARER } from "./bearer.enum.js";
-export { PUBLIC_ROUTES } from "./public-routes.enums.js";
+export { PUBLIC_ROUTES } from "./public-routes.enum.js";
 export { APIPath, AuthApiPath, HTTPMethod } from "@promptomat/shared";
