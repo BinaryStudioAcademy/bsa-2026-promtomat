16fbf2dd56789da5cbf174a356a5f926882ed7b8
Blackpossum <yosaphatharwindra@gmail.com>
2026-08-28 11:38:05 +0700

    refactor: remove unused CSS module and simplify illustration class assignment in NoAccessPage pm-19

--- apps/frontend/src/pages/no-access/no-access.tsx
+++ apps/frontend/src/pages/no-access/no-access.tsx
@@ -2,24 +2,20 @@ import noAccessIllustration from "~/assets/img/no-access.svg";
 import { FallbackScreen } from "~/libs/components/fallback-screen/fallback-screen.js";
 import { AppRoute, HTTPCode } from "~/libs/enums/enums.js";
 
-import styles from "./style.module.css";
-
 const NoAccessPage: React.FC = () => (
-	<main>
-		<FallbackScreen
-			action={{ label: "Back Home", url: AppRoute.ROOT }}
-			code={HTTPCode.FORBIDDEN}
-			illustration={
-				<img
-					alt="lock-illustration"
-					className={styles["illustration"]}
-					src={noAccessIllustration}
-				/>
-			}
-			message="Sorry, you are not allowed to access this page."
-			title="Forbidden"
-		/>
-	</main>
+	<FallbackScreen
+		action={{ label: "Back Home", url: AppRoute.ROOT }}
+		code={HTTPCode.FORBIDDEN}
+		illustration={
+			<img
+				alt="lock-illustration"
+				className={"illustration"}
+				src={noAccessIllustration}
+			/>
+		}
+		message="Sorry, you are not allowed to access this page."
+		title="Forbidden"
+	/>
 );
 
 export { NoAccessPage };
