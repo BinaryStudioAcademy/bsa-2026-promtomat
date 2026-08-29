cda299a3e495896468bc5f9b51c0d9277d70a1b5
Lesia Soloviova <solovyova.lesya@gmail.com>
2026-08-29 17:19:29 +0100

    feat: style the auth page pm-11

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -5,6 +5,8 @@ import { Link } from "~/libs/components/link/link.js";
 import { AppRoute } from "~/libs/enums/enums.js";
 import { useGetUsersQuery } from "~/modules/users/users-api.js";
 
+import styles from "./styles.module.css";
+
 const App: React.FC = () => {
 	const { pathname } = useLocation();
 
@@ -18,24 +20,22 @@ const App: React.FC = () => {
 
 	return (
 		<>
-			<img alt="logo" className="App-logo" src={reactLogo} width="30" />
+			<header className={styles["header"]}>
+				<img alt="logo" src={reactLogo} width="30" />
+				<ul className={styles["nav"]}>
+					<li>
+						<Link to={AppRoute.ROOT}>Root</Link>
+					</li>
+					<li>
+						<Link to={AppRoute.SIGN_IN}>Sign in</Link>
+					</li>
+					<li>
+						<Link to={AppRoute.SIGN_UP}>Sign up</Link>
+					</li>
+				</ul>
+			</header>
 
-			<ul className="App-navigation-list">
-				<li>
-					<Link to={AppRoute.ROOT}>Root</Link>
-				</li>
-				<li>
-					<Link to={AppRoute.SIGN_IN}>Sign in</Link>
-				</li>
-				<li>
-					<Link to={AppRoute.SIGN_UP}>Sign up</Link>
-				</li>
-			</ul>
-			<p>Current path: {pathname}</p>
-
-			<div>
-				<RouterOutlet />
-			</div>
+			<RouterOutlet />
 			{isRoot && (
 				<>
 					<h2>Users:</h2>
