9393b0ab1d003581a616a8971162cd14bffd05c9
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-09-01 22:47:53 +0300

    feat: add barrel exports for tech stack module pm-91

--- packages/shared/src/modules/workspaces/workspaces.ts
+++ packages/shared/src/modules/workspaces/workspaces.ts
@@ -4,6 +4,18 @@ export {
 	WorkspaceVisibility,
 } from "./libs/enums/enums.js";
 export {
+	getRandomTechStackTags,
+	getTechStackTagSuggestions,
+	isValidTechStackTag,
+	normalizeTagName,
+	normalizeTechStackTag,
+	normalizeTechStackTags,
+	TECH_STACK_DICTIONARY,
+	TECH_STACK_VARIANTS,
+	TechStackTagSchema,
+	TechStackTagsSchema,
+} from "./libs/modules/tech-stack-tags/tech-stack-tags.js";
+export {
 	type WorkspaceCreateRequestDto,
 	type WorkspaceDto,
 	type WorkspaceGetAllResponseDto,
