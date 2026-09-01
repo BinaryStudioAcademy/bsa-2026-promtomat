b5a89cee8c20be9dfc170d431442be603cf0f62e
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-09-01 22:46:29 +0300

    feat: add tech stack tag suggestions with random fallback, add barrel export pm-91

---
+++ packages/shared/src/modules/workspaces/libs/modules/tech-stack-tags/helpers/helpers.ts
@@ -0,0 +1,10 @@
+export {
+	isValidTechStackTag,
+	normalizeTagName,
+	normalizeTechStackTag,
+	normalizeTechStackTags,
+} from "./normalize.helper.js";
+export {
+	getRandomTechStackTags,
+	getTechStackTagSuggestions,
+} from "./suggest.helper.js";
