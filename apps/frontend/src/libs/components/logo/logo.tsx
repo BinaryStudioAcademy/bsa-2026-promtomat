88b3b210a50c8e264dcee2844ee2bcc31e3158dc
Blackpossum <yosaphatharwindra@gmail.com>
2026-09-03 10:07:52 +0700

    feat: replace temporary logo with SVG logo and update logo component pm-92

--- apps/frontend/src/libs/components/logo/logo.tsx
+++ apps/frontend/src/libs/components/logo/logo.tsx
@@ -1,3 +1,4 @@
+import logo from "~/assets/img/logo.svg";
 import { ControlSize } from "~/libs/enums/enums.js";
 import { getValidClasses } from "~/libs/helpers/helpers.js";
 import { type ValueOf } from "~/libs/types/types.js";
@@ -5,16 +6,17 @@ import { type ValueOf } from "~/libs/types/types.js";
 import styles from "./styles.module.css";
 
 type Properties = {
+	isIconOnly?: boolean;
 	size?: ValueOf<typeof ControlSize>;
 };
 
-const Logo: React.FC<Properties> = ({ size = ControlSize.MD }: Properties) => (
+const Logo: React.FC<Properties> = ({
+	isIconOnly = false,
+	size = ControlSize.MD,
+}: Properties) => (
 	<div className={getValidClasses(styles["logo"], styles[size])}>
-		{/* TODO pm-92 replace the temporary CSS mark with the final SVG logo */}
-		<span aria-hidden="true" className={styles["mark"]}>
-			P
-		</span>
-		<span className={styles["wordmark"]}>Promptomat</span>
+		<img alt="app logo" className={styles["mark"]} src={logo} />
+		{!isIconOnly && <span className={styles["wordmark"]}>Promptomat</span>}
 	</div>
 );
 
