ee8b6d366c7f77614a0f4c9039b29d2dfbddda84
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-09-04 01:20:32 +0300

    feat: export getTechStackTagSuggestions from shared package in frontend pm-91

--- apps/frontend/src/libs/helpers/helpers.ts
+++ apps/frontend/src/libs/helpers/helpers.ts
@@ -1,3 +1,4 @@
 export { capitalizeFirstLetter } from "./capitalize-first-letter/capitalize-first-letter.helper.js";
 export { checkIsDebugEnvironment } from "./check-is-debug-environment/check-is-debug-environment.helper.js";
 export { getValidClasses } from "./get-valid-classes/get-valid-classes.helper.js";
+export { getTechStackTagSuggestions } from "@promptomat/shared";
