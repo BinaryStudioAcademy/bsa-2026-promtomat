81068f118221bebdaf958d021da793d0fdfe9b7b
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2026-09-04 01:05:58 +0300

    refactor: remove static workspace stack tag options in favor of dynamic tags pm-91

--- apps/frontend/src/pages/workspaces/components/workspace-create-form/libs/constants/constants.ts
+++ apps/frontend/src/pages/workspaces/components/workspace-create-form/libs/constants/constants.ts
@@ -1,3 +1,2 @@
 export { DEFAULT_WORKSPACE_CREATE_PAYLOAD } from "./default-workspace-create-payload.constant.js";
 export { FIRST_ELEMENT_INDEX } from "./first-element-index.constant.js";
-export { WORKSPACE_STACK_TAG_OPTIONS } from "./workspace-stack-tag-options.constant.js";
