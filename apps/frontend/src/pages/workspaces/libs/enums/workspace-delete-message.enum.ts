af7a65667a3eb968eb23d746c705900067504bd3
Lesia Soloviova <solovyova.lesya@gmail.com>
2026-09-09 18:28:51 +0100

    fix: reword workspace deletion warning pm-70

--- apps/frontend/src/pages/workspaces/libs/enums/workspace-delete-message.enum.ts
+++ apps/frontend/src/pages/workspaces/libs/enums/workspace-delete-message.enum.ts
@@ -1,5 +1,5 @@
 const WorkspaceDeleteMessage = {
-	DELETION_CANNOT_BE_UNDONE: "Deleting this workspace cannot be undone.",
+	DELETION_CANNOT_BE_UNDONE: "Deleting the workspace cannot be undone.",
 	FAILURE: "The workspace was not deleted. Please try again.",
 	NO_PROMPTS: "This workspace has no prompts.",
 } as const;
