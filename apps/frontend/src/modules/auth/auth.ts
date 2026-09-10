e0e68a4afeeaf856b464c17d2eaf456dbe1176f5
Blackpossum <yosaphatharwindra@gmail.com>
2026-09-11 00:26:48 +0700

    feat: add validation schemas fpr forgot and reset password functionality pm-83

--- apps/frontend/src/modules/auth/auth.ts
+++ apps/frontend/src/modules/auth/auth.ts
@@ -4,10 +4,14 @@ export {
 } from "./libs/enums/enums.js";
 export { useSignOut } from "./libs/hooks/use-sign-out/use-sign-out.hook.js";
 export {
+	type ForgotPasswordRequestDto,
+	type NewPasswordFormValues,
 	type SignInRequestDto,
 	type SignUpRequestDto,
 } from "./libs/types/types.js";
 export {
+	forgotPasswordValidationSchema,
+	newPasswordValidationSchema,
 	passwordBoundarySpacesValidationSchema,
 	passwordLengthValidationSchema,
 	signInValidationSchema,
