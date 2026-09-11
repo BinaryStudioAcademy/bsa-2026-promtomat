d37a8b0ffc2303650c3b2f1a32eb1f3250b486b7
Blackpossum <yosaphatharwindra@gmail.com>
2026-09-11 18:36:21 +0700

    feat: remove PASSWORD_RESET_TOKENS from DatabaseTableName enum pm-83

--- apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
+++ apps/backend/src/libs/modules/database/libs/enums/database-table-name.enum.ts
@@ -1,6 +1,5 @@
 const DatabaseTableName = {
 	MIGRATIONS: "migrations",
-	PASSWORD_RESET_TOKENS: "password_reset_tokens",
 	PROMPT_EMBEDDINGS: "prompt_embeddings",
 	PROMPTS: "prompts",
 	USERS: "users",
