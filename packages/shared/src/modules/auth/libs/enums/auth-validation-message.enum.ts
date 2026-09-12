6bd48b2cf1f18414827d4c23aec1e889e3506bdb
Mykhailo Bekker <ipz211_bmm@student.ztu.edu.ua>
2026-09-12 17:54:07 +0300

    fix: email and password limit sign in pm-157 (#170)
    * fix: email and password validation rules pm-157
    
    * chore: email min len error message pm-157
    
    * chore: email validation fixed pm-157

--- packages/shared/src/modules/auth/libs/enums/auth-validation-message.enum.ts
+++ packages/shared/src/modules/auth/libs/enums/auth-validation-message.enum.ts
@@ -3,6 +3,8 @@ import { AuthValidationRule } from "./auth-validation-rule.enum.js";
 const AuthValidationMessage = {
 	EMAIL_INVALID: "Email is invalid",
 	EMAIL_REQUIRED: "Email is required",
+	EMAIL_TOO_LONG: `Email must be at most ${String(AuthValidationRule.EMAIL_MAXIMUM_LENGTH)} characters`,
+	EMAIL_TOO_SHORT: `Email must be at least ${String(AuthValidationRule.EMAIL_MINIMUM_LENGTH)} characters`,
 	NICKNAME_ALREADY_EXISTS: "This nickname is already taken",
 	NICKNAME_INVALID:
 		"Nickname can contain only Latin letters, numbers, and underscores, and must include at least one Latin letter",
