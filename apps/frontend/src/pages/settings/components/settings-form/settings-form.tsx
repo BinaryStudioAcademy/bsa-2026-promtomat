16b4d72d02f8cdb8e27eace680cc85844d9a66f9
Viktor Medvid <darkissdark@gmail.com>
2026-09-15 11:57:22 +0200

    fix: show nickname placeholder when the settings field is empty pm-206

--- apps/frontend/src/pages/settings/components/settings-form/settings-form.tsx
+++ apps/frontend/src/pages/settings/components/settings-form/settings-form.tsx
@@ -82,6 +82,7 @@ const SettingsForm: React.FC<Properties> = ({ user }: Properties) => {
 						label="Nickname"
 						maxLength={AuthValidationRule.NICKNAME_MAXIMUM_LENGTH}
 						name="nickname"
+						placeholder={SettingsMessage.NICKNAME_PLACEHOLDER}
 						size={ControlSize.LG}
 					/>
 					<Select
